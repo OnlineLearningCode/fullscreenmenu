48f1805e7170cbbd7cfbce053aac41e8fcb20194
OnlineLearningCode <63599676+OnlineLearningCode@users.noreply.github.com>
2020-08-04 17:18:38 +0500

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,10 @@
+
+function changeIcon() {
+  $('.icon').addClass('fa-times');
+  $('.icon').removeClass('fa-bar');
+}
+
+  function changeIcon2() {
+  $('.icon').addClass('fa-bar');
+  $('.icon').removeClass('fa-times');
+}
